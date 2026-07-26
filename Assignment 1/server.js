ca0c334f3f3641b553b28a910c3e353d7be2e7ca
Salma-kabel <salma.s.kabel@gmail.com>
2026-07-26 16:51:25 +0300

    Stage 2: read endpoints with 404

--- Assignment 1/server.js
+++ Assignment 1/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const port = 3000;
-
+const tasks = [
+    {id: 1, title: 'task1', done: true},
+    {id: 2, title: 'task2', done: false},
+    {id : 3, title: 'task3', done: true}
+]
 
 app.get('/', (req, res) => {
     res.json({ "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] });
@@ -10,6 +14,20 @@ app.get('/', (req, res) => {
 
 app.get('/health', (req, res) => {
     res.json({ "status": "ok" });
+});
+
+app.get('/tasks', (req, res) => {
+    return res.json(tasks);
+});
+
+app.get('/tasks/:id', (req, res) => {
+    task = tasks.find(t => t.id == req.params.id);
+    if (task) {
+        return res.json(task);
+    }
+    return res.status(404).json({
+        "error": `Task ${req.params.id} not found`
+    });
 });
 
 
